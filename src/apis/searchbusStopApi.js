0255399d97b00149d4f3852ccf9ad874103b50c0
elsa <elsa2700@gmail.com>
2021-12-02 01:14:14 +0800

    附近站牌頁面調整預設縣市為雙北

--- src/apis/searchbusStopApi.js
+++ src/apis/searchbusStopApi.js
@@ -2,23 +2,23 @@ import axios from "axios";
 import { getAuthorizationHeader } from "./axios";
 const doBusStopSearch = async (lat, lon) => {
   let res = null;
-  let city = null;
+  let city = "Taipei";
 
   try {
-    await axios
-      .get(
-        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=AIzaSyAEhhfNoyyTNN9F2NGIpscafVX4aJ5w0KE`
-      )
-      .then(function (response) {
-        city =
-          response.data.results[0].address_components[4].long_name.split(
-            " "
-          )[0];
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-
+    // await axios
+    //   .get(
+    //     `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lon}&key=AIzaSyAEhhfNoyyTNN9F2NGIpscafVX4aJ5w0KE`
+    //   )
+    //   .then(function (response) {
+    //     city =
+    //       response.data.results[0].address_components[4].long_name.split(
+    //         " "
+    //       )[0];
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+    console.log(city);
     await axios
       .get(
         `https://ptx.transportdata.tw/MOTC/v2/Bus/Station/City/${city}?$spatialFilter=nearby(${lat},${lon},1000)&$format=JSON`,
